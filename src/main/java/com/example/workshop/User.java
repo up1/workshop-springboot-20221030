b05a7751684c385ebe2b0d21f6afc642ddea7c84
up1 <somkiat.p@gmail.com>
2022-10-31 11:11:23 +0700

    Demo with create bean

---
+++ src/main/java/com/example/workshop/User.java
@@ -0,0 +1,8 @@
+package com.example.workshop;
+
+import org.springframework.stereotype.Component;
+
+public class User {
+    private Long id;
+    private String name;
+}
--- src/main/java/com/example/workshop/WorkshopApplication.java
+++ src/main/java/com/example/workshop/WorkshopApplication.java
@@ -3,9 +3,16 @@ package com.example.workshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class WorkshopApplication {
+
+//	@Bean
+//	public User xxx() {
+//		return new User();
+//	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context
