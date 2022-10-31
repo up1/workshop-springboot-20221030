3c465244c0e05ba60f1a15fcd0176342b532940d
up1 <somkiat.p@gmail.com>
2022-10-31 16:19:32 +0700

    Use MockBean and Mockito

--- src/test/java/com/example/workshop/users/UserControllerWebMvcTest.java
+++ src/test/java/com/example/workshop/users/UserControllerWebMvcTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,8 +21,20 @@ public class UserControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void getUserById() throws Exception {
+        // Arrange
+        Header header = new Header(200, "ok");
+        Body body = new Body(1);
+        UserResponse expected = new UserResponse();
+        expected.setHeader(header);
+        expected.setBody(body);
+        when(userService.getById(1)).thenReturn(expected);
+
+        // Act
         MvcResult mvcResult =
                 this.mvc.perform(get("/users/1").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
