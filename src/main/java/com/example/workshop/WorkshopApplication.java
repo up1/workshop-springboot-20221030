952cc8f5623531218861e88d2bdbb2e5039ed48e
up1 <somkiat.p@gmail.com>
2022-10-31 10:29:13 +0700

    Get all beans

--- src/main/java/com/example/workshop/WorkshopApplication.java
+++ src/main/java/com/example/workshop/WorkshopApplication.java
@@ -2,12 +2,19 @@ package com.example.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WorkshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(WorkshopApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
