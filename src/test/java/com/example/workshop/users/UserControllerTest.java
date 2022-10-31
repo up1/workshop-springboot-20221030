f3d4a1fda779d4551a2aad6e9b62c99dd3398f0b
up1 <somkiat.p@gmail.com>
2022-10-31 14:15:08 +0700

    Create a first test class

---
+++ src/test/java/com/example/workshop/users/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.workshop.users;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserControllerTest {
+
+    @Test
+    @DisplayName("การทดสอบดึงข้อมูลผู้ใช้งาน ....")
+    public void success_case() {
+    }
+}
