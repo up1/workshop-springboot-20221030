a2c7cce4df2269ca02b5613bc7996baef54c8eef
up1 <somkiat.p@gmail.com>
2022-10-31 11:29:16 +0700

    Working with bean's scope

--- src/main/java/com/example/workshop/User.java
+++ src/main/java/com/example/workshop/User.java
@@ -1,8 +1,28 @@
 package com.example.workshop;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class User {
     private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/workshop/WorkshopApplication.java
+++ src/main/java/com/example/workshop/WorkshopApplication.java
@@ -22,6 +22,15 @@ public class WorkshopApplication {
 			System.out.println(bean);
 		}
 		System.out.println(context.getBeanDefinitionCount());
+
+		User u1 = new User();
+		User u2 = context.getBean(User.class);
+		u2.setName("Name U2");
+		User u3 = context.getBean(User.class);
+		u3.setName("Name U3");
+
+		System.out.println(u2.getName());
+		System.out.println(u3.getName());
 	}
 
 }
