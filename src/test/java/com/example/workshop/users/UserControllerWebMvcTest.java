d4cb7a8ee4c9a3951314da7f1d44ddf766fb6551
up1 <somkiat.p@gmail.com>
2022-10-31 14:39:27 +0700

    Working with WebMvcTest

--- src/test/java/com/example/workshop/users/UserControllerWebMvcTest.java
+++ src/test/java/com/example/workshop/users/UserControllerWebMvcTest.java
@@ -1,9 +1,11 @@
 package com.example.workshop.users;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(UserController.class)
 public class UserControllerWebMvcTest {
 
     @Test
