17ed1f7837dfdb82c18c3ab17493b24a47c0182f
up1 <somkiat.p@gmail.com>
2022-11-01 15:28:45 +0700

    Use wiremock in component testing

--- src/test/java/com/example/workshop/todos/TodoGatewayComponentTest.java
+++ src/test/java/com/example/workshop/todos/TodoGatewayComponentTest.java
@@ -1,14 +1,17 @@
 package com.example.workshop.todos;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.MediaType;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 9999)
@@ -19,11 +22,23 @@ public class TodoGatewayComponentTest {
 
     @Test
     public void getById() {
+        // Arrange
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/todos/1"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody("{\n" +
+                                "  \"userId\": 1,\n" +
+                                "  \"id\": 1,\n" +
+                                "  \"title\": \"From Wiremock\",\n" +
+                                "  \"completed\": false\n" +
+                                "}"))
+        );
         // Act
         Optional<TodoResponse> result = todoGateway.getById(1);
         //Assert
         assertTrue(result.isPresent());
         assertEquals(1, result.get().getId());
-        assertEquals("delectus aut autem", result.get().getTitle());
+        assertEquals("From Wiremock", result.get().getTitle());
     }
 }
