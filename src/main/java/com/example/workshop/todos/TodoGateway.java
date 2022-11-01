70a8c93a73add4bd98ad5da7310414217990842f
up1 <somkiat.p@gmail.com>
2022-11-01 14:40:28 +0700

    Config targer url service

--- src/main/java/com/example/workshop/todos/TodoGateway.java
+++ src/main/java/com/example/workshop/todos/TodoGateway.java
@@ -1,5 +1,6 @@
 package com.example.workshop.todos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -12,12 +13,15 @@ public class TodoGateway {
 
     private RestTemplate restTemplate;
 
+    @Value("${target.url}")
+    private String targetUrl;
+
     public TodoGateway(RestTemplateBuilder builder){
         restTemplate = builder.build();
     }
 
     public Optional<TodoResponse> getById(int id) {
-        String url = String.format("https://jsonplaceholder.typicode.com/todos/%d", id);
+        String url = String.format("%s/todos/%d", targetUrl, id);
         try {
             TodoResponse result = restTemplate.getForObject(url,
                     TodoResponse.class);
