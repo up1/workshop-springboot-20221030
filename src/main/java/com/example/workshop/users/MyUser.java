28b48f2ae607c11774dd3aa602ea61dba4fcc7d6
up1 <somkiat.p@gmail.com>
2022-11-01 10:27:42 +0700

    Create repository with Spring Data JPA

---
+++ src/main/java/com/example/workshop/users/MyUser.java
@@ -0,0 +1,36 @@
+package com.example.workshop.users;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MyUser {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
